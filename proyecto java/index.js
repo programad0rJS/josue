5f5a01ef7018fa47cba150fb963f0eae3a645ae3
josue <83614619+josuefeo123@users.noreply.github.com>
2021-05-05 17:14:14 -0500

    subiendo
    subiendo

---
+++ proyecto java/index.js
@@ -0,0 +1,8 @@
+//const button = document.getElementById("button")
+
+/*button.addEventListener("click",() =>{
+    console.log("CLICK");
+})*/
+//button.addEventListener("dblclick",()=>{
+//    console.log("DOBLE CLICK")
+//})
